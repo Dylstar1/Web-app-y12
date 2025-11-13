7dece333ee845cb5758ea1c715e822a07eb14778
Dylstar1 <133065795+Dylstar1@users.noreply.github.com>
2025-11-13 12:14:25 +1100

    secured + logout

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, session, redirect, url_for, flash
 import sqlite3
+from datetime import date # for default date
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key'  # For sessions and flash messages
@@ -14,11 +15,66 @@ def get_db_connection():
 @app.route('/')
 def index():
     # return 'Index page'
-    return render_template('index.html')
+    return render_template('dashboard.html')
+
+@app.route('/addprogress', methods = ['GET', 'POST'])
+def addprogress():
+    if 'user_id' not in session:
+        flash('Please login, you need to login to view this content', 'error')
+        return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        date = request.form['date']
+        title = request.form['title']
+        details = request.form['details']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                f"INSERT INTO progresslogs (user_id, date, title, details) VALUES ('{session['user_id']}', '{date}', '{title}', '{details}')"
+            )
+            conn.commit()
+            flash('Log succesful!', ' success')
+            conn.close()
+            # give feedback to user 
+            # redirect to dashboard -- later ti view logs instead
+        except sqlite3.IntegrityError:
+            conn.close()
+            flash('Please enter a complere log.', 'error')
+            return redirect(url_for('addprogress'))
+        except Exception as e:
+            conn.close()
+            flash(f'Error: {str(e)}', 'error')
+            return redirect(url_for('addprogress'))
+        
+    return render_template('addprogress.html')
 
 @app.route('/dashboard')
 def dashboard():
+    if 'user_id' not in session:
+        flash('Please login, you need to login to view this content', 'error')
+        return redirect(url_for('login'))
+                        
     return render_template('dashboard.html')
+
+@app.route('/logs', methods = ['GET', 'POST'])
+def logs():
+    if 'user_id' not in session:
+        flash('Please login, you need to login to view this content', 'error')
+        return redirect(url_for('login'))
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT date, title, details FROM progresslogs WHERE user_id = '{session['user_id']}' ORDER BY date ASC" )
+    posts = cursor.fetchall()
+    conn.close()
+    # display no posts if user has none
+    # display all post is user has dynamic - for
+    
+    return render_template('logs.html', posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -31,6 +87,7 @@ def login():
         cursor.execute(f"SELECT * FROM users WHERE username = '{username}' AND passward = '{password}'")
         user = cursor.fetchone()
         conn.close()
+
         print('checked sql')
         if user:
             print('user')
@@ -74,6 +131,7 @@ def register():
         except sqlite3.IntegrityError:
             conn.close()
             flash('Username or email already exists', 'error')
+            return redirect(url_for('register'))
         except Exception as e:
             conn.close()
             flash(f'Error: {str(e)}', 'error')
